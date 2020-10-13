7875f06d67fc3cbf5049314864f319122a0c22e8
mar1n <30738216+mar1n@users.noreply.github.com>
2020-10-13 14:09:07 +0100

    using react router dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React, { Fragment } from "react";
 import Layout from "./core/Layout";
-import Layour from "./core/Layout";
 
 const App = () => {
-  const nav = () => (
-    <ul className="nav nav-tabs bg-primary">
-      <li className="nav-item">
-        <a href="/" className="text-light nav-link">
-          Home
-        </a>
-      </li>
-    </ul>
-  );
+
   return (
     <Fragment>
-      {nav()}
       <Layout>
         <h1>Hello React</h1>
       </Layout>
--- src/core/Layout.js
+++ src/core/Layout.js
@@ -1,9 +1,18 @@
 import React, { Fragment } from "react";
-
+import {Link} from "react-router-dom";
 const Layout = ({ children }) => {
+    const nav = () => (
+        <ul className="nav nav-tabs bg-primary">
+          <li className="nav-item">
+            <Link href="/" className="text-light nav-link">
+              Home
+            </Link>
+          </li>
+        </ul>
+      );
   return (
     <Fragment>
-      <p>this is nav</p>
+      {nav()}
       <div className="container">{children}</div>
     </Fragment>
   );
