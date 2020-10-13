2e115146e5167d51d980fbdfacd9040f06b78c1a
mar1n <30738216+mar1n@users.noreply.github.com>
2020-10-13 13:54:30 +0100

    create layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Layout from "./core/Layout";
+import Layour from "./core/Layout";
 
 const App = () => {
+  const nav = () => (
+    <ul className="nav nav-tabs bg-primary">
+      <li className="nav-item">
+        <a href="/" className="text-light nav-link">
+          Home
+        </a>
+      </li>
+    </ul>
+  );
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <Fragment>
+      {nav()}
+      <Layout>
+        <h1>Hello React</h1>
+      </Layout>
+    </Fragment>
   );
 };
 
---
+++ src/core/Layout.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <Fragment>
+      <p>this is nav</p>
+      <div className="container">{children}</div>
+    </Fragment>
+  );
+};
+
+export default Layout;
