f4df4fcc2b056718300d81ce9738e55a664ca4c8
mar1n <30738216+mar1n@users.noreply.github.com>
2020-10-13 15:11:30 +0100

    signup form

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -6,10 +6,65 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/react-toastify.esm";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "ryan",
+    email: "szym0nd4widowicz@gmail.com",
+    password: "rrrrrrr",
+    buttonText: "Submit",
+  });
+  const handleChange = (name) => (event) => {
+    //
+  };
+
+  const { name, email, password, buttonText } = values;
+  const clickSubmit = (event) => (
+    //
+    <></>
+  );
+  const signupFrom = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          value={name}
+          type="text"
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          value={email}
+          type="email"
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Passwrod</label>
+        <input
+          onChange={handleChange("password")}
+          value={password}
+          type="password"
+          className="form-control"
+        />
+      </div>
+
+      <button className="btn btn-primary" onClick={clickSubmit}>
+        {buttonText}
+      </button>
+    </form>
+  );
   return (
     <Layout>
-      <ToastContainer />
-      <h1>Signup</h1>
+      <div className="col-d-6 offset-md-3">
+        <ToastContainer />
+        <h1 className="p-5 text-center">Signup</h1>
+        {signupFrom()}
+      </div>
     </Layout>
   );
 };
